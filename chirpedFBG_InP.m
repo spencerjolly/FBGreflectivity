a49d4feb8212961eea47853b18a1b7ea4a1bc782
Spencer Jolly <spencer.jolly@vub.be>
2021-05-19 09:18:52 +0200

    add standard FBG and phase-shifted FBG files

--- chirpedFBG_InP.m
+++ chirpedFBG_InP.m
@@ -3,14 +3,14 @@ kappa_0 = 50e2;
 L = 200e-6;
 n = 3.266;
 lambda_0 = 1550e-9;
-BW = 3e-9;
+BW = 2e-9;
 kappa_0_L = kappa_0*L;
 F = (BW*4*pi*n*L)/(lambda_0^2);
 
 xmesh = linspace(-0.5, 0.5, 500);
 solinit = bvpinit(xmesh, @guess);
 
-Delta_scan = linspace(-15, 15, 500);
+Delta_scan = linspace(-25, 25, 500);
 r = zeros(1, length(Delta_scan));
 
 for i=1:length(Delta_scan)
@@ -25,7 +25,7 @@ end
 %%
 subplot(1, 2, 1)
 plot(Delta_scan, abs(r).^2)
-xlim([-15, 15])
+%xlim([-15, 15])
 xlabel('Delta')
 ylim([0, 1])
 ylabel('R')
@@ -37,7 +37,7 @@ subplot(1, 2, 2)
 plot(Delta_scan, phase)
 hold on
 plot(Delta_scan, p(1)*Delta_scan.^2 + Delta_scan*p(2) + p(3))
-xlim([-15, 15])
+%xlim([-15, 15])
 xlabel('Delta')
 ylabel('Phase [rad.]')
 
